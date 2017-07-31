434e3867025adb600da79b4fd728185a2da15ec3
lovecr4ft <haudema@gmail.com>
2017-07-31 18:07:01 +0200

    Bonne solution
    Last change

--- Exercices/Exercice1.cpp
+++ Exercices/Exercice1.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main()
 {
   int sum=0;
-  for(int i=1;i<1001;i++){
+  for(int i=1;i<1000;i++){
 	  if(i%5==0||i%3==0){
 	  sum+=i;
 	  }
 	  
   }
   cout<<sum<<endl;
-  // Answer is 234168
+  // Answer is 233168
   return 0;
 }
 
